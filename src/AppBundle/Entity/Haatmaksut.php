daa10562a881a8e13e8c4f4b6df161d5dfb95156
Ari Ikäläinen <ari.ikalainen@hotmail.com>
2016-11-22 22:05:12 +0100

    Doctrine v2

--- src/AppBundle/Entity/Haatmaksut.php
+++ src/AppBundle/Entity/Haatmaksut.php
@@ -14,7 +14,7 @@ class Haatmaksut
 	
 	/**
      * @ORM\Column(type="integer")
-     * @ORM\Nmaksutid
+     * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $nmaksutid;
@@ -28,4 +28,62 @@ class Haatmaksut
      * @ORM\Column(type="decimal", scale=2)
      */
     private $narvio;
+
+    /**
+     * Get nmaksutid
+     *
+     * @return integer
+     */
+    public function getNmaksutid()
+    {
+        return $this->nmaksutid;
+    }
+
+    /**
+     * Set ckuvaus
+     *
+     * @param string $ckuvaus
+     *
+     * @return Haatmaksut
+     */
+    public function setCkuvaus($ckuvaus)
+    {
+        $this->ckuvaus = $ckuvaus;
+
+        return $this;
+    }
+
+    /**
+     * Get ckuvaus
+     *
+     * @return string
+     */
+    public function getCkuvaus()
+    {
+        return $this->ckuvaus;
+    }
+
+    /**
+     * Set narvio
+     *
+     * @param string $narvio
+     *
+     * @return Haatmaksut
+     */
+    public function setNarvio($narvio)
+    {
+        $this->narvio = $narvio;
+
+        return $this;
+    }
+
+    /**
+     * Get narvio
+     *
+     * @return string
+     */
+    public function getNarvio()
+    {
+        return $this->narvio;
+    }
 }
