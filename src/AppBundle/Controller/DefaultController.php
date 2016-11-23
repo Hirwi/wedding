17c92813f68800d507644ea3601599c743484713
Ari Ikäläinen <ari.ikalainen@hotmail.com>
2016-11-23 22:06:22 +0100

    Data from and to DB Test

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use AppBundle\Entity\Haatmaksut;
+use Symfony\Component\HttpFoundation\Response;
+
 class DefaultController extends Controller
 {
     /**
@@ -24,4 +27,74 @@ class DefaultController extends Controller
 			'labels' => $this->labels[$lang]
         ]);
     }
+	
+	
+	// malli tietokantaan laitto entityn avulla
+	public function createAction()
+	{
+		$haatmaksut = new Haatmaksut();
+		$haatmaksut->setCkuvaus('Kuvaustesti1');
+		$haatmaksut->setNarvio(12345);
+
+		$em = $this->getDoctrine()->getManager();
+		
+		// tells Doctrine you want to (eventually) save the Product (no queries yet)
+		$em->persist($haatmaksut);
+
+		// actually executes the queries (i.e. the INSERT query)
+		$em->flush();
+
+		return new Response('Saved new product with id '.$haatmaksut->getNmaksutid());
+		exit;
+	}
+	
+	
+	
+	public function showAction($productId)
+	{
+		
+		$haatid = 4;
+		$repository = $this->getDoctrine()
+			->getRepository('AppBundle:Haatmaksut');
+			
+			
+		$haatmaksut = $repository->find($haatid);
+		
+		$haatmaksut = $repository->findAll();
+
+		if (!$haatmaksut) {
+			throw $this->createNotFoundException(
+				'No product found for id '.$haatid
+			);
+		}
+		exit;
+		
+		$repository = $this->getDoctrine()->getRepository('AppBundle:Product');
+
+		// query for a single product by its primary key (usually "id")
+		$product = $repository->find($productId);
+
+		// dynamic method names to find a single product based on a column value
+		$product = $repository->findOneById($productId);
+		$product = $repository->findOneByName('Keyboard');
+
+		// dynamic method names to find a group of products based on a column value
+		$products = $repository->findByPrice(19.99);
+
+		// find *all* products
+		$products = $repository->findAll();
+		
+		
+		//echo $haatmaksut->nmaksutid;
+		echo "<pre>";
+		print_r($haatmaksut);
+		exit;
+		
+		
+		
+		
+	}	
+
+	
+	
 }
--- src/AppBundle/Entity/Haatmaksut.php
+++ src/AppBundle/Entity/Haatmaksut.php
@@ -28,7 +28,23 @@ class Haatmaksut
      * @ORM\Column(type="decimal", scale=2)
      */
     private $narvio;
+	
+	/**
+     * @ORM\Column(type="decimal", scale=2)
+     */
+    private $ntoteutunut;
 
+	/**
+     * @ORM\Column(type="text")
+     */
+    private $ckategoria;
+	
+	/**
+     * @ORM\Column(type="integer")
+     */
+    private $nhuomioilaskennassa;
+	
+	
     /**
      * Get nmaksutid
      *
@@ -85,5 +101,77 @@ class Haatmaksut
     public function getNarvio()
     {
         return $this->narvio;
+    }
+
+    /**
+     * Set ntoteutunut
+     *
+     * @param string $ntoteutunut
+     *
+     * @return Haatmaksut
+     */
+    public function setNtoteutunut($ntoteutunut)
+    {
+        $this->ntoteutunut = $ntoteutunut;
+
+        return $this;
+    }
+
+    /**
+     * Get ntoteutunut
+     *
+     * @return string
+     */
+    public function getNtoteutunut()
+    {
+        return $this->ntoteutunut;
+    }
+
+    /**
+     * Set ckategoria
+     *
+     * @param string $ckategoria
+     *
+     * @return Haatmaksut
+     */
+    public function setCkategoria($ckategoria)
+    {
+        $this->ckategoria = $ckategoria;
+
+        return $this;
+    }
+
+    /**
+     * Get ckategoria
+     *
+     * @return string
+     */
+    public function getCkategoria()
+    {
+        return $this->ckategoria;
+    }
+
+    /**
+     * Set nhuomioilaskennassa
+     *
+     * @param integer $nhuomioilaskennassa
+     *
+     * @return Haatmaksut
+     */
+    public function setNhuomioilaskennassa($nhuomioilaskennassa)
+    {
+        $this->nhuomioilaskennassa = $nhuomioilaskennassa;
+
+        return $this;
+    }
+
+    /**
+     * Get nhuomioilaskennassa
+     *
+     * @return integer
+     */
+    public function getNhuomioilaskennassa()
+    {
+        return $this->nhuomioilaskennassa;
     }
 }
